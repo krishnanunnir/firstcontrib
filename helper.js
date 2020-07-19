968a5dba7721a36169b2b22a18ab7915a7f15105
Krishnanunni R <krishnanunnir97@gmail.com>
2020-07-19 17:36:12 +0530

    [enhancement] Added code to find issues added in the last day

--- helper.js
+++ helper.js
@@ -6,6 +6,26 @@ function returnRepoName(json){
     return repoArray;
 }
 
+function returnRepoIssue(json){
+    //Method to return the repo full name from the json
+
+    const repoArray = json.map((issue)=>{
+        createdDate = new Date(issue['created_at']);
+        var lastHour = new Date( Date.now() - 86400000 )
+        if(createdDate > lastHour ){
+            return {
+                'title':issue["title"],
+                'description':issue["body"],
+                'url': issue["html_url"]
+            };
+        }else{
+            return null;
+        }
+    });
+    return repoArray;
+}
+
 module.exports={
-    returnRepoName: returnRepoName
+    returnRepoName: returnRepoName,
+    returnRepoIssue: returnRepoIssue
 }
--- index.js
+++ index.js
@@ -1,15 +1,65 @@
 const fetch = require("node-fetch");
-const { returnRepoName } = require("./helper");
+const { returnRepoName, returnRepoIssue } = require("./helper");
 
 let apiUrl = "https://api.github.com";
 let userName = "krishnanunnir"
+let authToken = "token <token>"
 let starsUrl = apiUrl + "/users/" + userName +"/starred";
-
-repos = []
-fetch(starsUrl).then((result) =>{
+var lastHour = new Date( Date.now() - 3600000 ).toISOString();
+const myHeaders = new fetch.Headers({
+    'Authorization': authToken
+});
+const myRequest = new fetch.Request(starsUrl, {
+    method: 'GET',
+    headers: myHeaders,
+    mode: 'cors',
+    cache: 'default',
+});
+fetch(myRequest).
+then((result) =>{
     return result.json();
 }).then((result)=>{
     repos =  returnRepoName(result);
-    console.log(repos);
+    repos.forEach(element => {
+        urlIssues = apiUrl+"/repos/"+element+"/issues";
+        fetchIssues(urlIssues, lastHour);
+    });
+})
+.catch((error)=>{
+    console.log(error);
 });
 
+
+function fetchIssues(urlIssues, timeSince){
+    const myHeaders = new fetch.Headers({
+        'Authorization': authToken,
+        'Accept': 'application/vnd.github.v3+json',
+        'state': 'open',
+        'sorted': 'created',
+        'since': timeSince
+    });
+    const myRequest = new fetch.Request(urlIssues, {
+        method: 'GET',
+        headers: myHeaders,
+        mode: 'cors',
+        cache: 'default',
+    });
+    fetch(myRequest)
+    .then((result)=>{
+        return result.json();
+    })
+    .then((data)=>{
+        issues = returnRepoIssue(data);
+        issues.forEach(element => {
+            if(element){
+                console.log(element["title"]);
+                console.log(element["url"]);
+            }
+
+        });
+
+    })
+    .catch((error)=>{
+        console.log(error);
+    });
+}
