b7907f776511871d2b6dfc35cdfd60aa5b110b28
Krishnanunni R <krishnanunnir97@gmail.com>
2020-07-22 02:00:55 +0530

    Modified templating to show the issues

--- routes/home.js
+++ routes/home.js
@@ -4,12 +4,12 @@ const routes = express.Router();
 const githubHandle = require("../github-api");
 
 routes.get('/',(req,res,next)=>{
-    res.render(path.join(__dirname,'..','views','index.ejs'));
+    res.render(path.join(__dirname,'..','views','index.ejs'),{issues:[]});
 });
 
 routes.post('/',async(req,res,next)=>{
-    let val = await githubHandle(req.body)
-    res.send(val);
+    let issues = await githubHandle(req.body)
+    res.render(path.join(__dirname,'..','views','index.ejs'),{issues:issues});
 });
 
 module.exports = routes;
