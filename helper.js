e276c5e672896b68db13e98d57614248542cf436
Krishnanunni R <krishnanunnir97@gmail.com>
2020-07-22 13:12:43 +0530

    [ui] Updated frontend to look better and render markdown

--- helper.js
+++ helper.js
@@ -1,3 +1,27 @@
+var md = require('markdown-it')({
+    html:         true,        // Enable HTML tags in source
+    xhtmlOut:     false,        // Use '/' to close single tags (<br />).
+                                // This is only for full CommonMark compatibility.
+    breaks:       false,        // Convert '\n' in paragraphs into <br>
+    langPrefix:   'language-',  // CSS language prefix for fenced blocks. Can be
+                                // useful for external highlighters.
+    linkify:      false,        // Autoconvert URL-like text to links
+   
+    // Enable some language-neutral replacement + quotes beautification
+    typographer:  false,
+   
+    // Double + single quotes replacement pairs, when typographer enabled,
+    // and smartquotes on. Could be either a String or an Array.
+    //
+    // For example, you can use '«»„“' for Russian, '„“‚‘' for German,
+    // and ['«\xA0', '\xA0»', '‹\xA0', '\xA0›'] for French (including nbsp).
+    quotes: '“”‘’',
+   
+    // Highlighter function. Should return escaped HTML,
+    // or '' if the source string is not changed and should be escaped externally.
+    // If result starts with <pre... internal wrapper is skipped.
+    highlight: function (/*str, lang*/) { return ''; }
+  });
 function returnRepoName(json){
     //Method to return the repo full name from the json
     const repoArray = json.map((repo)=>{
@@ -6,7 +30,7 @@ function returnRepoName(json){
     return repoArray;
 }
 
-function returnRepoIssue(json, timeBack = 7*86400000){
+function returnRepoIssue(json, timeBack = 86400000){
     //Method to return the repo full name from the json
     const repoArray = json.reduce((filtered, issue)=>{
         createdDate = new Date(issue['created_at']);
@@ -14,7 +38,7 @@ function returnRepoIssue(json, timeBack = 7*86400000){
         if(createdDate > lastHour ){
             filtered.push({
                 'title':issue["title"],
-                'description':issue["body"],
+                'description':md.render(issue["body"]),
                 'url': issue["html_url"]
             });
         }
