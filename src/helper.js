e9cc4eaccdc06686e68dba4100c0fd4808be7cc7
Krishnanunni R <krishnanunnir97@gmail.com>
2020-07-24 22:22:36 +0530

    [refactor] Removed some comments from the code

--- src/helper.js
+++ src/helper.js
@@ -1,25 +1,11 @@
 var md = require('markdown-it')({
-    html:         true,        // Enable HTML tags in source
-    xhtmlOut:     false,        // Use '/' to close single tags (<br />).
-                                // This is only for full CommonMark compatibility.
-    breaks:       false,        // Convert '\n' in paragraphs into <br>
-    langPrefix:   'language-',  // CSS language prefix for fenced blocks. Can be
-                                // useful for external highlighters.
-    linkify:      false,        // Autoconvert URL-like text to links
-   
-    // Enable some language-neutral replacement + quotes beautification
+    html:         true,      
+    xhtmlOut:     false,      
+    breaks:       false,      
+    langPrefix:   'language-',
+    linkify:      false,      
     typographer:  false,
-   
-    // Double + single quotes replacement pairs, when typographer enabled,
-    // and smartquotes on. Could be either a String or an Array.
-    //
-    // For example, you can use '«»„“' for Russian, '„“‚‘' for German,
-    // and ['«\xA0', '\xA0»', '‹\xA0', '\xA0›'] for French (including nbsp).
     quotes: '“”‘’',
-   
-    // Highlighter function. Should return escaped HTML,
-    // or '' if the source string is not changed and should be escaped externally.
-    // If result starts with <pre... internal wrapper is skipped.
     highlight: function (/*str, lang*/) { return ''; }
   });
 
