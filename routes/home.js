3ec8f2d7242202e45068a4d9b050e2277219fb46
Krishnanunni R <krishnanunnir97@gmail.com>
2020-07-26 02:30:07 +0530

    [enhancement] Added handling when no result and link preview values

--- routes/home.js
+++ routes/home.js
@@ -11,12 +11,15 @@ routes.post('/',async(req,res,next)=>{
     let issues = [];
     try{
         repoIssues = await githubHandle(req.body)
-        res.render(path.join(__dirname,'..','views','index.ejs'),{issues:repoIssues,error:""});
+        if(repoIssues && repoIssues.length){
+            res.render(path.join(__dirname,'..','views','index.ejs'),{issues:repoIssues,error:""});
+        }else{
+            res.render(path.join(__dirname,'..','views','index.ejs'),{issues:[],error:"Sorry! We couldn't find any issues. Try searching longer back."});
+        }
     }
     catch(error)
     {
-        console.log(error.message);
-        res.render(path.join(__dirname,'..','views','index.ejs'),{issues:[],error:error.message});
+        res.render(path.join(__dirname,'..','views','index.ejs'),{issues:[],error:"We couldn't find what you were searching for"});
     }
 });
 
