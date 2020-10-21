39b5f8dfbb0af47ae5b378ccd7f9e4d1697a80d8
masayaShinoda <masayashida@gmail.com>
2020-10-21 18:20:57 +0700

    style changes, made site responsive

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 const router = require('./routes/home');
 const bodyParser = require("body-parser");
 app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));   // directory that serves additional scss
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(router);
 app.listen(process.env.PORT,() => {
